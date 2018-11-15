acd8de45118a736b4dcacf73363b545535f9a4cd
skmbw <tongku2008@126.com>
2018-11-15 16:08:31 +0800

    ecql语句查询，页面参数验证

--- src/app/query/query.component.ts
+++ src/app/query/query.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ToastrService} from 'ngx-toastr';
 import {Query} from '../bean/Query';
+import {DatabaseService} from '../service/database.service';
 
 @Component({
   selector: 'app-query',
@@ -11,14 +12,36 @@ export class QueryComponent implements OnInit {
   catalog = '22';
   tableName = '11';
   query: Query = new Query();
+  tableList = [{'name': 'newgdelt2', 'catalog': 'gdelt2'}, {'name': 'newgdelt3', 'catalog': 'gdelt2'}];
 
-  constructor(private toastr: ToastrService) {
+  constructor(private toastr: ToastrService, private database: DatabaseService) {
   }
 
   ngOnInit() {
   }
 
   submit() {
-    this.toastr.success(this.catalog);
+    if (this.query.catalog === undefined) {
+      this.toastr.success('请选择数据库');
+      return;
+    }
+    if (this.query.tableName === undefined) {
+      this.toastr.success('请选择要查询的表');
+      return;
+    }
+    if (this.query.ecql === undefined || this.query.ecql.trim() === '') {
+      this.toastr.success('ECQL语句不能为空');
+      return;
+    }
+    // this.database.post('table/query', this.query).subscribe(result => {});
+  }
+
+  catalogChange($event) {
+    // this.toastr.success($event);
+    if ($event === 'gdelt') {
+      this.tableList.push({'catalog': 'mysql', 'name': 'user'});
+    } else {
+      this.tableList.push({'catalog': 'oracle', 'name': 'roles'});
+    }
   }
 }
