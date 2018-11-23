609982b91ccd376920d6113d3afa0bf315e5cb7a
skmbw <tongku2008@126.com>
2018-11-23 10:15:52 +0800

    动态表格，以及显示动态列。从后端返回的数据中，提取列数据和要显示的数据

--- src/app/bean/JsUtils.ts
+++ src/app/bean/JsUtils.ts
@@ -14,4 +14,29 @@ export class JsUtils {
     }
     return false;
   }
+
+  static toQueryString(obj: any): string {
+    let rs = '';
+    for (const p in obj) {
+      // for in statement must filtered with an if statement
+      if (obj.hasOwnProperty(p)) {
+        const v = obj[p];
+        if (v !== undefined && v !== null) {
+          rs += p + '=' + v + '&';
+        }
+      }
+    }
+    return rs.substring(0, rs.length - 1);
+  }
+
+  static getColumn(obj: any): string[] {
+    const rs: string[] = [];
+    for (const p in obj) {
+      // for in statement must filtered with an if statement
+      if (obj.hasOwnProperty(p)) {
+        rs.push(p);
+      }
+    }
+    return rs;
+  }
 }
--- src/app/query/query.component.ts
+++ src/app/query/query.component.ts
@@ -4,9 +4,7 @@ import {Query} from '../bean/Query';
 import {DatabaseService} from '../service/database.service';
 import {JsUtils} from '../bean/JsUtils';
 import {HttpClient} from '@angular/common/http';
-import {merge, Observable, of} from 'rxjs';
 import {MatPaginator, MatTableDataSource} from '@angular/material';
-import {catchError, map, startWith, switchMap} from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-query',
@@ -17,53 +15,68 @@ export class QueryComponent implements OnInit {
   @Input() catalog = '';
   @Input() tableName = '';
   query: Query = new Query();
-  displayedColumns = ['created', 'state', 'number', 'title'];
-  exampleDatabase: ExampleHttpDao | null;
-  data: MatTableDataSource<GithubIssue> = null;
+  // displayedColumns = ['created', 'state', 'number', 'title'];
+  displayedColumns = [];
+  // exampleDatabase: ExampleHttpDao | null;
+  data: MatTableDataSource<any> = null;
   // data: MatTableDataSource<GithubIssue[]> = {};
   // data: GithubIssue[] = [];
 
   resultsLength = 0;
-  isLoadingResults = true;
-  isRateLimitReached = false;
+  isLoadingResults = false;
+  // isRateLimitReached = false;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+
   constructor(private toastr: ToastrService, private database: DatabaseService,
               private http: HttpClient) {
   }
 
   ngOnInit() {
-    this.exampleDatabase = new ExampleHttpDao(this.http);
+    // this.exampleDatabase = new ExampleHttpDao(this.http);
     // this.data.paginator = this.paginator;
     // If the user changes the sort order, reset back to the first page.
     // this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
 
     // this.exampleDatabase.getRepoIssues('true', 'asc', this.paginator.pageIndex).subscribe(
     //   data => this.data = new MatTableDataSource<GithubIssue[]>(data));
-    merge(this.paginator.page)
-      .pipe(
-        startWith({}),
-        switchMap(() => {
-          this.isLoadingResults = true;
-          return this.exampleDatabase.getRepoIssues('true', 'asc', this.paginator.pageIndex);
-        }),
-        map(data => {
-          // Flip flag to show that loading has finished.
-          this.isLoadingResults = false;
-          this.isRateLimitReached = false;
-          this.resultsLength = data.total_count;
 
-          return data.items;
-        }),
-        catchError(() => {
-          this.isLoadingResults = false;
-          // Catch if the GitHub API has reached its rate limit. Return empty data.
-          this.isRateLimitReached = true;
-          return of([]);
-        })
-      ).subscribe(data => {
-        this.data = new MatTableDataSource<GithubIssue>(data);
-      }); // this.data = data
+    // merge(this.paginator.page)
+    //   .pipe(
+    //     startWith({}),
+    //     switchMap(() => {
+    //       if (JsUtils.isAnyBlank(this.catalog, this.tableName, this.query.ecql)) {
+    //         const js = new JsonBean();
+    //         js.code = 10;
+    //         return of<JsonBean>(js);
+    //       }
+    //       this.isLoadingResults = true;
+    //       // return this.exampleDatabase.getRepoIssues('true', 'asc', this.paginator.pageIndex);
+    //       this.query.catalog = this.catalog;
+    //       this.query.tableName = this.tableName;
+    //       return this.database.post('table/query', this.query);
+    //     }),
+    //     map(data => {
+    //       // Flip flag to show that loading has finished.
+    //       this.isLoadingResults = false;
+    //       if (data.code === 10) {
+    //         return [];
+    //       }
+    //       // this.isRateLimitReached = false;
+    //       this.resultsLength = data.data.length; // data.total_count;
+    //
+    //       // return data.items;
+    //       return data.data;
+    //     }),
+    //     catchError(() => {
+    //       this.isLoadingResults = false;
+    //       // Catch if the GitHub API has reached its rate limit. Return empty data.
+    //       // this.isRateLimitReached = true;
+    //       return of([]);
+    //     })
+    //   ).subscribe(data => {
+    //   this.data = new MatTableDataSource<any>(data);
+    // }); // this.data = data
   }
 
   submit() {
@@ -79,33 +92,48 @@ export class QueryComponent implements OnInit {
       this.toastr.success('ECQL语句不能为空');
       return;
     }
+    this.isLoadingResults = true;
     this.query.catalog = this.catalog;
     this.query.tableName = this.tableName;
-    // this.database.post('table/query', this.query).subscribe(result => {});
+    this.database.post('table/query', this.query).subscribe(result => {
+      // this.data = new MatTableDataSource<any>(result.data);
+      this.isLoadingResults = false;
+      const resultArray: any[] = [];
+      const jsonArray = JSON.parse(result.data);
+      for (const bean of jsonArray) {
+        resultArray.push(bean.properties);
+      }
+      if (resultArray.length > 0) {
+        const column = resultArray[0];
+        this.displayedColumns = JsUtils.getColumn(column);
+      }
+      this.data = new MatTableDataSource<any>(resultArray);
+    });
   }
 }
 
-export interface GithubApi {
-  items: GithubIssue[];
-  total_count: number;
-}
-
-export interface GithubIssue {
-  created_at: string;
-  number: string;
-  state: string;
-  title: string;
-}
-
-/** An example database that the data source uses to retrieve data for the table. */
-export class ExampleHttpDao {
-  constructor(private http: HttpClient) {}
-
-  getRepoIssues(sort: string, order: string, page: number): Observable<GithubApi> {
-    const href = 'https://api.github.com/search/issues';
-    const requestUrl =
-      `${href}?q=repo:angular/material2&sort=${sort}&order=${order}&page=${page + 1}`;
-
-    return this.http.get<GithubApi>(requestUrl);
-  }
-}
+//
+// export interface GithubApi {
+//   items: GithubIssue[];
+//   total_count: number;
+// }
+//
+// export interface GithubIssue {
+//   created_at: string;
+//   number: string;
+//   state: string;
+//   title: string;
+// }
+//
+// /** An example database that the data source uses to retrieve data for the table. */
+// export class ExampleHttpDao {
+//   constructor(private http: HttpClient) {}
+//
+//   getRepoIssues(sort: string, order: string, page: number): Observable<GithubApi> {
+//     const href = 'https://api.github.com/search/issues';
+//     const requestUrl =
+//       `${href}?q=repo:angular/material2&sort=${sort}&order=${order}&page=${page + 1}`;
+//
+//     return this.http.get<GithubApi>(requestUrl);
+//   }
+// }
